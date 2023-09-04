54898f7295eadab01ce50e036e3549b9d140900c
Anabel Huanaco <anabelhuanaco@gmail.com>
2023-09-04 17:08:51 -0300

    Ejercicios insert

---
+++ EjerciciosModificacion/Ejercicio1.sql
@@ -0,0 +1,8 @@
+INSERT INTO user (username, password)
+VALUES ("Anabel", 1234);
+
+INSERT INTO post (author_id, title,body)
+VALUES(1, "Ocean","el oceano y las medusas");
+
+INSERT INTO post (author_id, title,body)
+VALUES(1, "Mar","burbujas");
